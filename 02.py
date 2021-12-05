65b739150e8483f300dca7bd622172cd4e0a2afb
Breno Arosa <brenoarosa@hotmail.com>
2021-12-04 21:30:12 -0300

    Adds solution to second day of puzzles

---
+++ 02.py
@@ -0,0 +1,49 @@
+from typing import List, Tuple
+
+
+def get_final_position(instructions: List[Tuple[str, int]]) -> Tuple[int, int]:
+    x = 0
+    y = 0
+
+    for cmd, value in instructions:
+        if cmd == "forward":
+            x += value
+        elif cmd == "up":
+            y -= value
+        elif cmd == "down":
+            y += value
+        else:
+            raise Exception("Unexpect value")
+
+    return x, y
+
+def get_corrected_final_position(instructions: List[Tuple[str, int]]) -> Tuple[int, int]:
+    x = 0
+    y = 0
+    aim = 0
+
+    for cmd, value in instructions:
+        if cmd == "forward":
+            x += value
+            y += aim * value
+        elif cmd == "up":
+            aim -= value
+        elif cmd == "down":
+            aim += value
+        else:
+            raise Exception("Unexpect value")
+
+    return x, y
+
+
+
+if __name__ == '__main__':
+    with open("input_02.txt") as fin:
+        lines = fin.readlines()
+    instructions = [line.rstrip().split(" ") for line in lines]
+    instructions = [(instruction[0], int(instruction[1])) for instruction in instructions]
+
+    x, y = get_final_position(instructions)
+    print(x * y)
+    x, y = get_corrected_final_position(instructions)
+    print(x * y)
